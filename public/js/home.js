a659e880467eec773f18bb772eab9be89508f037
Caio Silva <caiosilvakss@gmail.com>
2022-12-18 23:25:01 -0300

    integração com a API

--- public/js/home.js
+++ public/js/home.js
@@ -1,3 +1,5 @@
+const main = document.querySelector('.main')
+
 fetch(genres_list_http + new URLSearchParams({
     api_key: api_key
 }))
@@ -14,5 +16,45 @@ const fetchMoviesListByGenres = (id, genres) => {
         page: Math.floor(Math.random() * 3) + 1
     }))
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => {
+            makeCategoryElement(`${genres}_movies`, data.results)
+        })
+        .catch(err => console.log(err))
+}
+
+
+const makeCategoryElement = (category, data) => {
+    main.innerHTML += ` 
+    
+<div class="movie-list">
+    <button class="prev-btn">
+        <img src="img/prev.png" alt="previous button">
+    </button>
+    <h1 class="movie-category">${category.replace('_', ' ')}</h1>
+    <div class="movie-container" id="${category}"></div>
+        <button class="next-btn">
+            <img src="img/next.png" alt="next button">
+        </button>
+</div>
+    `
+    makeCards(category, data)
+}
+
+const makeCards = (id, data) => {
+    const movieContainer = document.getElementById(id)
+
+    data.forEach((item, i) => {
+        if (item.backdrop_path == null) {
+            item.backdrop_path = item.poster_path;
+            if (item.backdrop_path == null) {
+
+            }
+        }
+        movieContainer.innerHTML += `
+        <div class="movie">
+        <img src="${img_url}${item.backdrop_path}" alt="poster-1">
+        <p class="movie-title">${item.title}</p>
+    </div>
+    `
+    })
 }
